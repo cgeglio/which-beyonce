662864b231c656b7ddafa4daea2580ed35069d2a
Carla Geglio <carla.geglio@gmail.com>
2019-11-01 11:48:09 -0600

    Add error message if empty input

--- main.js
+++ main.js
@@ -4,23 +4,38 @@ var playerOne = document.querySelector(".input-one");
 var playerTwo = document.querySelector(".input-two");
 var nameOne = document.querySelector(".name-one");
 var nameTwo = document.querySelector(".name-two");
-playBtn.disabled = true;
+// var inputArea = document.querySelector(".player-names");
+// playBtn.disabled = true;
+
+
 playBtn.addEventListener("click", showDirections);
 playerOne.addEventListener("keyup", checkInputs);
+//
+// function checkInputs() {
+//   if (playerOne.value) {
+//     // playBtn.disabled = false;
+//     playBtn.id = "active";
+// } else
+//
+//   return;
+// }
+
 
 function checkInputs() {
   if (playerOne.value) {
-    playBtn.disabled = false;
     playBtn.id = "active";
-}
-}
-
+  }
+};
 
 function showDirections() {
-  document.querySelector(".player-names").style.display = "none";
-  welcomeMsg.style.display = "block";
-  nameOne.innerText = `${playerOne.value.toUpperCase()}`;
+  if (playBtn.id === "active") {
+    document.querySelector(".player-names").style.display = "none";
+    welcomeMsg.style.display = "block";
+    nameOne.innerText = `${playerOne.value.toUpperCase()}`;
   if (playerTwo.value) {
     nameTwo.innerText = ` AND ${playerTwo.value.toUpperCase()}`;
-    }
   }
+} else {
+    document.querySelector(".error").style.display = "block";
+  }
+};
