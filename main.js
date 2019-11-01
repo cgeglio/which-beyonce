5315cb3e0bbbef1867f30a0920047df23807d917
Carla Geglio <carla.geglio@gmail.com>
2019-11-01 11:22:56 -0600

    Allow one or two names to be entered and format welcome message for one player and two players

--- main.js
+++ main.js
@@ -4,13 +4,23 @@ var playerOne = document.querySelector(".input-one");
 var playerTwo = document.querySelector(".input-two");
 var nameOne = document.querySelector(".name-one");
 var nameTwo = document.querySelector(".name-two");
-
+playBtn.disabled = true;
 playBtn.addEventListener("click", showDirections);
+playerOne.addEventListener("keyup", checkInputs);
+
+function checkInputs() {
+  if (playerOne.value) {
+    playBtn.disabled = false;
+    playBtn.id = "active";
+}
+}
 
 
 function showDirections() {
   document.querySelector(".player-names").style.display = "none";
   welcomeMsg.style.display = "block";
   nameOne.innerText = `${playerOne.value.toUpperCase()}`;
-  nameTwo.innerText = `${playerTwo.value.toUpperCase()}`;
-}
+  if (playerTwo.value) {
+    nameTwo.innerText = ` AND ${playerTwo.value.toUpperCase()}`;
+    }
+  }
