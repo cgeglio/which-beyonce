1a02df321d84c3ae67d1c84226bf58bd54797b85
Carla Geglio <carla.geglio@gmail.com>
2019-11-01 11:08:13 -0600

    Add player name to welcome page

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 var playBtn = document.querySelector(".play-btn");
+var welcomeMsg = document.querySelector(".welcome-msg")
+var playerOne = document.querySelector(".input-one");
+var playerTwo = document.querySelector(".input-two");
+var nameOne = document.querySelector(".name-one");
+var nameTwo = document.querySelector(".name-two");
 
 playBtn.addEventListener("click", showDirections);
 
 
 function showDirections() {
   document.querySelector(".player-names").style.display = "none";
-  document.querySelector(".welcome-msg").style.display = "block";
+  welcomeMsg.style.display = "block";
+  nameOne.innerText = `${playerOne.value.toUpperCase()}`;
+  nameTwo.innerText = `${playerTwo.value.toUpperCase()}`;
 }
