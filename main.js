370e36c482aa8ac9c838931de4c4801695ae3a35
Carla Geglio <carla.geglio@gmail.com>
2019-11-01 18:33:00 -0600

    Input player name on game-play screen

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ var sidebars = document.querySelector(".sidebar");
 var cardContainer = document.querySelector(".card");
 var main = document.querySelector("main");
 var gamePage = document.querySelector(".game-page");
-var nameOne = document.querySelector(".name-one");
-var nameTwo = document.querySelector(".name-two");
+var nameOne = document.querySelectorAll(".name-one");
+var nameTwo = document.querySelectorAll(".name-two");
 
 playBtnMain.addEventListener("click", showDirections);
 playerOne.addEventListener("keyup", checkInputs);
@@ -18,6 +18,7 @@ playBtnWelcome.addEventListener("click", showGame);
 function showGame() {
   main.style.display = "none";
   gamePage.style.display = "flex";
+  // nameOne.innerText = "jhkgugi";
 }
 
 function checkInputs() {
@@ -29,12 +30,17 @@ function checkInputs() {
 function showDirections() {
   if (playBtnMain.id === "active") {
     document.querySelector(".player-names").style.display = "none";
+    console.log(nameOne);
     welcomeMsg.style.display = "block";
-    nameOne.innerText = `${playerOne.value.toUpperCase()}`;
+    for (var i = 0; i < nameOne.length; i++) {
+    nameOne[i].innerText = `${playerOne.value.toUpperCase()}`;
+    }
   if (playerTwo.value) {
-    nameTwo.innerText = ` AND ${playerTwo.value.toUpperCase()}`;
+    for (var i = 0; i < nameTwo.length; i++) {
+    nameTwo[i].innerText = ` AND ${playerTwo.value.toUpperCase()}`;
   }
 } else {
     document.querySelector(".error").style.display = "block";
   }
+}
 };
